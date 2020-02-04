7109a040a10bc7b971cc335c0224f9e3f54196a4
rujenamatya <rru.jju@gmail.com>
2020-02-03 20:10:24 -0500

    Yes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 let app = new Vue({
     el: "#vueApp",
     data: {
-        welcomeMessage: 'Hello World!'
+        //welcomeMessage: 'TODO Items',
+        title: 'Simple TODO list',
+        lists: [
+            {id:1, item:'complete assignment'},
+            {id:2, item:'submit homework'},
+            {id:3, item:'play game'}
+        ],
+        newItem: '',
     },
     methods: {
+        addItem:function(){
+            let id = this.lists.length + 1
+            if (this.newItem !== ''){
+                const newList = {id:id, item:this.newItem}
+                this.lists.push(newList)
+                this.newItem = ''
+            }
+        },
+        removeItem:function(){
+            
+        }
 
     },
     mounted() {
